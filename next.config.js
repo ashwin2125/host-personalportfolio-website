1ad97515e9c359a61a832e99fe89ebb69d04fc8b
Ashwin Gopalsamy <ashwin.gopalsamy.extern@solarisbank.de>
2023-08-27 12:41:01 +0530

    minor update

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
+const nextConfig = {
     images: {
       remotePatterns: [
         {
           protocol: 'https',
           hostname: 'images.credly.com',
-          port: '',
-          pathname: '/images/2abf0ede-58a3-4031-a05c-b4de0088a18f/blob.jpeg',
         },
       ],
     },
-  }
+  };
+
+
+  module.exports = nextConfig;
