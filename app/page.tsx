44b014e2401a63330eb579e4b171a5a6482519c6
Ashwin Gopalsamy <ashwin.gopalsamy.extern@solarisbank.de>
2024-08-05 15:31:21 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
       <About />
       <SectionDivider />
       <Skills />
+      <SectionDivider />
       <Experience />
       <Projects />
       <SectionDivider />
