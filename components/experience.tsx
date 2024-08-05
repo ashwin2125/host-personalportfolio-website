1ded8be003291aee64ad1355c687fef6c8904a16
Ashwin Gopalsamy <ashwin.gopalsamy.extern@solarisbank.de>
2024-08-05 15:08:10 +0530

    ....

--- components/experience.tsx
+++ components/experience.tsx
@@ -23,6 +23,7 @@ export default function Experience() {
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
             <VerticalTimelineElement
+             className="custom-timeline-element"
               contentStyle={{
                 background:
                   theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
